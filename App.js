4d8c53e24db176ad3b6ebdbadb1dbc20f14b74c7
Praveen-BE <praveenrajendiran3939@gmail.com>
2024-07-28 17:13:35 +0530

    functional Component

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import dog from "./public/images/dog3.jpg";
+// import dog from "./public/images/dog3.jpg";
 
-const heading = React.createElement("h1",{className: "heading"}, "Hello React 🍌🙏😊😂🤣");
-console.log(heading);
-const jsxHeading = ([
-<h1 className="head" tabIndex={1}>Hello JSX React ❤️</h1>,
-<h2 className="heading">Hello Boy</h2>,
-<img Src={dog} height="200x" width="200px"/>]
+
+const Title = ()=> (
+    <h1 className="heading">Namaste React👌</h1>
 );
-console.log(jsxHeading);
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title/>
+        <h1 className="head">Namaste React Functional Component😊</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(jsxHeading);
+
+root.render(<HeadingComponent/>);
