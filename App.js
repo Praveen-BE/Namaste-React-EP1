24ee44598d2e64c0ae5febc540074eaf90955930
Praveen-BE <praveenrajendiran3939@gmail.com>
2024-07-28 16:53:14 +0530

    IgnitingApp

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-const heading = React.createElement("h1",{class: "heading"}, "Hello React");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import dog from "./public/images/dog3.jpg";
+
+const heading = React.createElement("h1",{className: "heading"}, "Hello React 🍌🙏😊😂🤣");
+console.log(heading);
+const jsxHeading = ([
+<h1 className="head" tabIndex={1}>Hello JSX React ❤️</h1>,
+<h2 className="heading">Hello Boy</h2>,
+<img Src={dog} height="200x" width="200px"/>]
+);
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(jsxHeading);
---
+++ README.md
@@ -0,0 +1,29 @@
+# Namaste React
+
+
+
+
+
+# parcel
+    - Dev Build
+    - Local Server
+    - HMR - Hot Module Replacement
+    - File Watching algorithm written in C++
+    - caching - Faster Builds
+    - Image Optimization
+    - minification
+    - Bundling
+    - Compressing
+    - Consistent Hashing
+    - code splitting
+    - Difential Bunling
+    - Diagnostic
+    - Error Handling
+    - Https
+    - Tree shaking - Remove Unused Code
+    - Lazy Mode
+    - Different dev and prod bundle
+
+# babel
+    - jsx to React.createElement
+
